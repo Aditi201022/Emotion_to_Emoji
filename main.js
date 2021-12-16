1ef971f835cbc2cedc88bf441155f9baf96fa067
Aditi201022 <79974541+Aditi201022@users.noreply.github.com>
2021-12-16 16:43:36 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -24,6 +24,43 @@ function modelLoaded() {
     console.log('Model Loaded!')
 }
 
+function check(){
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results){
+    if (error) {
+        console.error(error);
+    } else {
+        console.log(results);
+        document.getElementById("result_emotion_name").innerHTML = results[0].label;
+        document.getElementById("result_emotion_name2").innerHTML = results[1].label;
+        prediction_1 = results[0].label;
+        prediction_2 = results[1].label;
+        speak();
+        if (results[0].lable == "happy") {
+            document.getElementById("update_emoji").innerHTML = "&#128522;";            
+        }
+        if (results[0].lable == "sad") {
+            document.getElementById("update_emoji").innerHTML = "&#128532;";         
+        }
+        if (results[0].lable == "angry") {
+            document.getElementById("update_emoji").innerHTML = "&#128548;";            
+        }
+
+        if (results[1].lable == "happy") {
+            document.getElementById("update_emoji2").innerHTML = "&#128522;";            
+        }
+        if (results[1].lable == "sad") {
+            document.getElementById("update_emoji2").innerHTML = "&#128532;";         
+        }
+        if (results[1].lable == "angry") {
+            document.getElementById("update_emoji2").innerHTML = "&#128548;";            
+        }
+    }
+}
+
 function speak(){
     var synth = windo.speechSynthesis;
     speak_data_1 = "The first predicttion is " + prediction_1;
