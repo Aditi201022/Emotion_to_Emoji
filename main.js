374ff7406ef3a23f2ea734a495c0a2e6c64a9134
Aditi201022 <79974541+Aditi201022@users.noreply.github.com>
2021-12-16 17:08:40 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,30 +1,43 @@
+prediction_1 = ""
+prediction_2 = ""
+
 Webcam.set({
     width:350,
     height:300,
-    image_format:'png',
+    image_format : 'png',
     png_quality:90
-});
+  });
 
 camera = document.getElementById("camera");
 
 Webcam.attach('#camera');
 
+      
 function take_snapshot()
 {
     Webcam.snap(function(data_uri) {
-        document.getElementById("result").innerHTML = '<img id = "captured_image" src="'+data_uri+'"/>';
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
     });
-
 }
 
-console.log('ml5 version:', ml5.version);
-classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/j3KQagA8l/model.json' ,modelLoaded);
+  console.log('ml5 version:', ml5.version);
+  
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/j3KQagA8l/model.json',modelLoaded);
 
-function modelLoaded() {
-    console.log('Model Loaded!')
+  function modelLoaded() {
+    console.log('Model Loaded!');
+  }
+  
+function speak(){
+  var synth = window.speechSynthesis;
+  speak_data_1 = "The first prediction is " + prediction_1;
+  speak_data_2 = "And the second prediction is " + prediction_2;
+  var utterThis = new SpeechSynthesisUtterance(speak_data_1 + speak_data_2);
+  synth.speak(utterThis);
 }
 
-function check()
+
+  function check()
   {
     img = document.getElementById('captured_image');
     classifier.classify(img, gotResult);
@@ -70,14 +83,5 @@ function gotResult(error, results) {
 }
 
 
-function speak(){
-    var synth = window.speechSynthesis;
-    speak_data_1 = "The first predicttion is " + prediction_1;
-    speak_data_2 = "And the second prediction is " + prediction_2;
-    var utterThis = new SpeechSynthesisUtterance(speak_data_1 + speak_data_2);
-    synth.speak(utterThis);
-}
 
 
-
-/*https://teachablemachine.withgoogle.com/models/j3KQagA8l/*/
